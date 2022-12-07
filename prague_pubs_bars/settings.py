248ffaee8b78b7e488dbf01ea5935b9a07eb910f
vojtech <p.prusavojta@gmail.com>
2022-12-07 14:25:27 +0100

    Update settings db

--- prague_pubs_bars/settings.py
+++ prague_pubs_bars/settings.py
@@ -81,23 +81,23 @@ WSGI_APPLICATION = 'prague_pubs_bars.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': config("DATABASE_NAME"),
-       'USER': config("DATABASE_USER"),
-       'PASSWORD': config("DATABASE_PASSWORD"),
-       'HOST': 'ec2-44-209-24-62.compute-1.amazonaws.com',
-       'PORT': '5432',
-   }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+# 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': config("DATABASE_NAME"),
+#        'USER': config("DATABASE_USER"),
+#        'PASSWORD': config("DATABASE_PASSWORD"),
+#        'HOST': 'ec2-44-209-24-62.compute-1.amazonaws.com',
+#        'PORT': '5432',
+#    }
+# }
 
 
 # Password validation
