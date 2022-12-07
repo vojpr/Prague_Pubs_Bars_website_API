55a0498365254c0c22ca86dfeddd6452e4b60981
vojtech <p.prusavojta@gmail.com>
2022-12-07 15:52:43 +0100

    Update settings db

--- prague_pubs_bars/settings.py
+++ prague_pubs_bars/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR, 'static']
 
 # Default primary key field type
